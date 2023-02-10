f92255785a38ffb9b10a7ec2d5d60f742e11d1f1
thesneilert <eilert.gaming@outlook.com>
2023-02-10 14:15:17 +0100

    added button positions and cap at map

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const appPicture = document.getElementById("appPicture")
 var currentRoom = [1,0]
 
 const map = [
-    ["b","c","d","j"],
-    ["a","ø","e",""],
-    ["h","g","f",""]
+    ["a1","a2","a3","a4"],
+    ["b1","","b3",""],
+    ["c1","c2","b3",""]
 ]
 
 
@@ -15,44 +15,44 @@ const map = [
 renderDungeonBg()
 function renderDungeonBg(){
     appPicture.innerHTML = /*html*/`
-    <img src="assets/dungeon/dungeon.png"></img>
+    <img src="assets/dungeon/dungeon.png">
     `;
 }
 
 //rendering the exits
 renderExit()
 function renderExit(){
-    //North
-    if (map[currentRoom[0]-1][currentRoom[1]]!=""){
+    //north
+    if (map[currentRoom[0]-1][currentRoom[1]]!="" && currentRoom[0]>0){
         appPicture.innerHTML += /*html*/`
-            <img class="north-exit" src="assets/dungeon/north.png"></img>
+            <img class="north-exit" src="assets/dungeon/north.png">
             `;
     }
     else{
 
     }
-    //South
-    if (map[currentRoom[0]+1][currentRoom[1]]!=""){
+    //south
+    if (map[currentRoom[0]+1][currentRoom[1]]!="" && currentRoom[0]<map.length){
         appPicture.innerHTML += /*html*/`
-        <img class="south-exit" src="assets/dungeon/south.png"></img>
+        <img class="south-exit" src="assets/dungeon/south.png">
         `;
     }
     else{
 
     }
-    //West
+    //west
     if (map[currentRoom[0]][currentRoom[1]-1]!=""){
         appPicture.innerHTML += /*html*/`
-        <img class="west-exit" src="assets/dungeon/west.png"></img>
+        <img class="west-exit" src="assets/dungeon/west.png">
         `;
     }
     else{
 
     }
-    //East
+    //east
     if (map[currentRoom[0]][currentRoom[1]+1]!=""){
         appPicture.innerHTML += /*html*/`
-        <img class="east-exit" src="assets/dungeon/east.png"></img>
+        <img class="east-exit" src="assets/dungeon/east.png">
         `;
     }
     else{
