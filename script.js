ae484b41eb4c5c16b625574740decf22407b87d4
thesneilert <eilert.gaming@outlook.com>
2023-02-10 21:46:57 +0100

    fixes and stats menu

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 // ----- MODEL -----
 const appPicture = document.getElementById("appPicture")
 
-var currentRoom = [0,0]
+const map = [    
+    ["A1","A2","A3","A4"],
+    ["B1","","B3",""],
+    ["C1","C2","C3",""]
+];
 
-const map = [
-    ["a1","a2","a3","a4"],
-    ["b1","","b3",""],
-    ["c1","c2","b3",""]
-]
+var currentRoom = [0,0];
+var currentExitAmount = 0;
+var currentRoomName = map[currentRoom[0]][currentRoom[1]];
+var currentGoldAmount = 0;
+var currentHealthAmount = 0;
+
+//always show 4 digits in statsmenu (0001, 0011, 0111, 1111)
+if (currentGoldAmount >= 0 && currentGoldAmount < 10) {
+    currentGoldAmount = "000" + currentGoldAmount;
+} else if (currentGoldAmount >= 10 && currentGoldAmount < 100) {
+    currentGoldAmount = "00" + currentGoldAmount;
+} else if (currentGoldAmount >= 100 && currentGoldAmount < 1000) {
+    currentGoldAmount = "0" + currentGoldAmount;
+}
+
+if (currentHealthAmount >= 0 && currentHealthAmount < 10) {
+    currentHealthAmount = "000" + currentHealthAmount;
+} else if (currentHealthAmount >= 10 && currentHealthAmount < 100) {
+    currentHealthAmount = "00" + currentHealthAmount;
+} else if (currentHealthAmount >= 100 && currentHealthAmount < 1000) {
+    currentHealthAmount = "0" + currentHealthAmount;
+}
+
 
 
 // ----- VIEW -----
@@ -27,6 +49,7 @@ function renderExit(){
         appPicture.innerHTML += /*html*/`
             <img class="north-exit" src="assets/dungeon/north.png"/>
             `;
+            currentExitAmount ++;
     }
     else{
 
@@ -36,6 +59,7 @@ function renderExit(){
         appPicture.innerHTML += /*html*/`
         <img class="south-exit" src="assets/dungeon/south.png"/>
         `;
+        currentExitAmount ++;
     }
     else{
 
@@ -45,6 +69,7 @@ function renderExit(){
         appPicture.innerHTML += /*html*/`
         <img class="west-exit" src="assets/dungeon/west.png"/>
         `;
+        currentExitAmount ++;
     }
     else{
 
@@ -54,11 +79,34 @@ function renderExit(){
         appPicture.innerHTML += /*html*/`
         <img class="east-exit" src="assets/dungeon/east.png"/>
         `;
+        currentExitAmount ++;
     }
     else{
 
     }
 }
 
+renderText()
+function renderText(){
+    appText.innerHTML += /*html*/`
+    You are in room ${currentRoomName}. <br>The room has ${currentExitAmount} exits. <br>Choose one to proceed.
+        `;
+}
+
+renderStats()
+function renderStats(){
+    appStats.innerHTML += /*html*/`
+        <div id="appStats">
+            <div class="stats-container">
+                <img class="gold-coin-stats" src="assets/stats_menu/gold_coin_0.png"/>
+                <span class="text-stats-gold">${currentGoldAmount}</span>
+            </div>
+            <div class="stats-container">
+                <img class="health-stats" src="assets/stats_menu/health_100.png"/>
+                <span class="text-stats-health">${currentHealthAmount}</span>
+            </div>
+        </div>
+        `;
+}
 
 // ----- CONTROLLER -----
